a272643defe33581f226cc383998df110a5a34a1
sphynx-henryay <pyc05079@gmail.com>
2021-06-29 22:52:23 +0800

    Moved workflows related into a package.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from typing import Dict, Optional
 
-from fastapi import FastAPI, Body, Response
+from fastapi import FastAPI, Body
 from dotenv import dotenv_values
 
 import knime
 
-settings = dotenv_values( ".env" )
+from . import workflows
 
+settings = dotenv_values( ".env" )
 
 env = {
 	"executable_path": settings[ "EXECUTABLE_PATH" ],
@@ -16,6 +17,7 @@ env = {
 knime.executable_path = env[ "executable_path" ]
 
 app = FastAPI()
+app.include_router( workflows.router )
 
 @app.get( "/env" )
 def get_env():
@@ -32,52 +34,3 @@ def set_env(
 	knime.executable_path = env[ "executable_path" ]
 	return env
 
-@app.get( "/workflows/" )
-def list_workflows():
-	import os
-
-	workspace_path = env[ "workspace_path" ]
-
-	def is_workflow( in_path ):
-		return True if "workflow.knime" in os.listdir( os.path.join( workspace_path, in_path ) ) else False
-
-	return {
-		p: { "is_workflow": is_workflow( p ) }
-		for p in os.listdir( workspace_path )
-		if "." in p
-	}
-
-@app.get( "/workflows/show/{workflow:path}" )
-def get_workflow( workflow: str ):
-	with knime.Workflow( workspace_path = env[ "workspace_path" ], workflow_path = workflow ) as wf:
-		return Response( wf._adjust_svg() )
-
-@app.post( "/workflows/run/{workflow:path}" )
-def run_workflow(
-	workflow: str,
-	data = Body(
-		...,
-		example = {
-			"1": {
-				"sepal length": 6.7,
-				"sepal width": 3,
-				"petal length": 6,
-				"petal width": 2
-			},
-			"2": {
-				"sepal length": 5.5,
-				"sepal width": 4.2,
-				"petal length": 1,
-				"petal width": 0.3
-			},
-		}
-	)
-):
-
-	from pandas import DataFrame
-
-	with knime.Workflow( workspace_path = env[ "workspace_path" ], workflow_path = workflow ) as wf:
-		wf.data_table_inputs[0] = DataFrame.from_dict( data, orient = "index" )
-		wf.execute()
-
-		return wf.data_table_outputs[0]
---
+++ workflows/__init__.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter, Body, Response
+
+router = APIRouter(
+	prefix = "/workflows",
+	tags = [ "workflows" ]
+)
+
+@router.get( "/" )
+def list_workflows():
+	import os
+
+	workspace_path = env[ "workspace_path" ]
+
+	def is_workflow( in_path ):
+		return True if "workflow.knime" in os.listdir( os.path.join( workspace_path, in_path ) ) else False
+
+	return {
+		p: { "is_workflow": is_workflow( p ) }
+		for p in os.listdir( workspace_path )
+		if "." in p
+	}
+
+@router.get( "/show/{workflow:path}" )
+def get_workflow( workflow: str ):
+	with knime.Workflow( workspace_path = env[ "workspace_path" ], workflow_path = workflow ) as wf:
+		return Response( wf._adjust_svg() )
+
+@router.post( "/run/{workflow:path}" )
+def run_workflow(
+	workflow: str,
+	data = Body(
+		...,
+		example = {
+			"1": {
+				"sepal length": 6.7,
+				"sepal width": 3,
+				"petal length": 6,
+				"petal width": 2
+			},
+			"2": {
+				"sepal length": 5.5,
+				"sepal width": 4.2,
+				"petal length": 1,
+				"petal width": 0.3
+			},
+		}
+	)
+):
+
+	from pandas import DataFrame
+
+	with knime.Workflow( workspace_path = env[ "workspace_path" ], workflow_path = workflow ) as wf:
+		wf.data_table_inputs[0] = DataFrame.from_dict( data, orient = "index" )
+		wf.execute()
+
+		return wf.data_table_outputs[0]
